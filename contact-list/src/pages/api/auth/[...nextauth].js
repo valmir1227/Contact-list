08d3837d1b01a8c22185ba738f3032a39091c8f9
Valmir Almeida <almeidavalmir76@gmail.com>
2023-03-01 21:46:51 -0300

    first commit

---
+++ contact-list/src/pages/api/auth/[...nextauth].js
@@ -0,0 +1,19 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import LinkedInProvider from "next-auth/providers/linkedin";
+
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    
+    LinkedInProvider({
+      clientId: process.env.LINKEDIN_CLIENT_ID,
+      clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
+    }),
+  ],
+};
+
+export default NextAuth(authOptions);
---
+++ contact-list/src/pages/signin/index.jsx
@@ -0,0 +1,10 @@
+import Login from "@/components/login-btn";
+import React from "react";
+
+export default function index() {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+}
